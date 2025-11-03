72eec6a57a8d2c872e41c1dbcc8a93179ece2515
Alvee3120 <fazlahkarim2002@gmail.com>
2025-11-03 13:36:55 +0600

    base

--- Templates_and_Static_files/myapp/urls.py
+++ Templates_and_Static_files/myapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 
-    path('index', views.index)
+    path('index',views.index),
+    path('<str:month>', views.monthly_challenges, name="monthly-challange")
 ]
--- Templates_and_Static_files/myapp/views.py
+++ Templates_and_Static_files/myapp/views.py
@@ -3,10 +3,42 @@ from django.template.loader import render_to_string
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
+
+monthly_challanges_dictionary = {
+        "january": "Plan your goals for the new year and start learning a new skill.",
+        "february": "Celebrate a holiday, like Lunar New Year or Valentine's Day, and catch up on indoor reading.",
+        "march": "Do some early spring cleaning and declutter your living space.",
+        "april": "Spend time outdoors, start your gardening project, or visit a local park.",
+        "may": None,
+        "june": "Enjoy the beginning of summer by attending a local outdoor festival or concert.",
+        "july": "Host a BBQ or picnic with friends and try out a new grilling recipe.",
+        "august": None,
+        "september": "Start a new fitness routine and embrace the cozy autumn transition.",
+        "october": "Visit a pumpkin patch, carve a jack-o'-lantern, or watch a spooky movie marathon.",
+        "november": "Reflect on what you're grateful for and volunteer for a charitable cause.",
+        "december": None
+}
+
+
+
+def monthly_challenges(request,month):
 
     # response_data = render_to_string('myapp/myapp.html')
     # return HttpResponse(response_data)
+    
+    monthly_advice = monthly_challanges_dictionary[month]
 
-    return render(request,"myapp/myapp.html")
+    return render(request,"myapp/myapp.html",{
+        "month_name" : month.capitalize(),
+        "advice" : monthly_advice
+    })
 
+def index(request):
+
+    months = list(monthly_challanges_dictionary.keys())
+
+    return render(request , "myapp/index.html", {
+
+        "months" : months
+
+    })
